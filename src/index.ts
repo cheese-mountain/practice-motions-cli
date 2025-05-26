508283f086053758e648c584e1b559c469b76051
kasperostberg <109141670+cheese-mountain@users.noreply.github.com>
2025-05-26 07:06:09 +0200

    adding difficulity levels

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
 #!/usr/bin/env bun
 
 import { confirm } from '@inquirer/prompts'
-import prompt from './prompt'
-import motions from './motions.json' assert { type: 'json' }
+import { prompt, log, getMotions } from './lib'
 
-function log(err: unknown) {
-    console.log(err instanceof Error ? err.message : err)
-}
-
-function shuffle<T>(array: Array<T>): Array<T> {
-    const shuffled = [...array]
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
-        ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
-    }
-    return shuffled
-}
 const options = { clearPromptOnDone: true }
+const motions = getMotions()
 
 const ready = await confirm({ message: 'Ready? 🏁' }).catch(log)
 if (ready) {
     const start = performance.now()
-    for (const motion of shuffle(motions)) {
+    for (const motion of motions) {
         await prompt(motion, options).catch(log)
     }
 
